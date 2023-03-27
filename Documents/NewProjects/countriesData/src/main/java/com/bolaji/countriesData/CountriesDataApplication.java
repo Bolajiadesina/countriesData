99b971a97cc0dbf8daf9fa169130ca1059882dd5
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-03-27 22:31:00 +0100

    adding packages

---
+++ Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/CountriesDataApplication.java
@@ -0,0 +1,13 @@
+package com.bolaji.countriesData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesDataApplication.class, args);
+	}
+
+}
