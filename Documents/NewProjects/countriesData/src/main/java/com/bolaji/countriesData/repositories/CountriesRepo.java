b9e0b9829934d909cc8bde1d397553496860fa01
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-04-01 00:13:58 +0100

    fix a broken dependency

--- Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/repositories/CountriesRepo.java
+++ Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/repositories/CountriesRepo.java
@@ -1,10 +1,14 @@
 package com.bolaji.countriesData.repositories;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.bolaji.countriesData.models.CountriesModel;
 import com.bolaji.countriesData.models.CountryRequest;
 
+
+
+@Service
 public interface CountriesRepo {
 
     public ResponseEntity<CountriesModel>  getAllCountries();
