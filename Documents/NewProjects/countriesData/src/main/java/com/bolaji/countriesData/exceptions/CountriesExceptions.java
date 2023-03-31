fc9ee5c8384fe755784b295caba56a957a25821b
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-03-31 23:09:28 +0100

    unit testing

--- Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/exceptions/CountriesExceptions.java
+++ Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/exceptions/CountriesExceptions.java
@@ -13,23 +13,23 @@ public class CountriesExceptions {
     private String message;
     private String code;
 
-    public CountriesExceptions(ResponseEnum responseEnum) {
-        super(responseEnum.getMessage());
-        this.responseEnum = responseEnum;
-        this.code = responseEnum.getCode();
-        this.message = responseEnum.getMessage();
-    }
+    // public CountriesExceptions(ResponseEnum responseEnum) {
+    //     super(responseEnum.getMessage());
+    //     this.responseEnum = responseEnum;
+    //     this.code = responseEnum.getCode();
+    //     this.message = responseEnum.getMessage();
+    // }
     
-    public CountriesExceptions(ResponseEnum responseEnum, String message) {
-        super((!StringUtils.isEmpty(message)) ? message : responseEnum.getMessage());
-        this.responseEnum = responseEnum;
-        this.code = responseEnum.getCode();
-        this.message = (!StringUtils.isEmpty(message) ? message : responseEnum.getMessage());
-    }
+    // public CountriesExceptions(ResponseEnum responseEnum, String message) {
+    //     super((!StringUtils.isEmpty(message)) ? message : responseEnum.getMessage());
+    //     this.responseEnum = responseEnum;
+    //     this.code = responseEnum.getCode();
+    //     this.message = (!StringUtils.isEmpty(message) ? message : responseEnum.getMessage());
+    // }
     
-    public CountriesExceptions(String code, String message) {
-        super(message);
-        this.code = code;;
-        this.message = message;
-    }
+    // public CountriesExceptions(String code, String message) {
+    //     super(message);
+    //     this.code = code;;
+    //     this.message = message;
+    // }
 }
