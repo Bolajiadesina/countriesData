a571b9c0e6776cd5da4eb2283aa9b7096a03e288
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-04-06 23:58:53 +0100

    adding the thymeleaf feature 2

--- Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/controllers/DemoController.java
+++ Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/controllers/DemoController.java
@@ -1,19 +1,15 @@
 package com.bolaji.countriesData.controllers;
 
-
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-
 public class DemoController {
-    @RequestMapping(value ="/portfolio")
+    @RequestMapping(value ="/bolaji")
      public String bolajiPage() {  
         
-      return "thymeleafTemplate";
+      return "bolaji";
    
      }
 }
