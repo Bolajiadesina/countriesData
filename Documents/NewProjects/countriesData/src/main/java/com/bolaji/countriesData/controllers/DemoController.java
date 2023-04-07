8144ab3cbaa1d9633633a648144081eecec3fa4b
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-04-07 19:24:12 +0100

    adding the thymeleaf

--- Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/controllers/DemoController.java
+++ Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/controllers/DemoController.java
@@ -2,14 +2,14 @@ package com.bolaji.countriesData.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class DemoController {
-    @RequestMapping(value ="/bolaji")
+    @GetMapping(value ="/bolaji")
      public String bolajiPage() {  
-        
+       
       return "bolaji";
-   
      }
 }
--- Documents/NewProjects/countriesData/src/test/java/com/bolaji/countriesData/CountryServiceImplTest.java
+++ Documents/NewProjects/countriesData/src/test/java/com/bolaji/countriesData/CountryServiceImplTest.java
@@ -2,15 +2,29 @@ package com.bolaji.countriesData;
 
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.bolaji.countriesData.controllers.DemoController;
+
 public class CountryServiceImplTest {
-    @Test 
-        public void getAllCountries() {
-    
+  public class HelpControllerTest {
+ 
+    private MockMvc mockMvc;
 
+    @BeforeAll
+    public void setup() {
+      mockMvc = MockMvcBuilders.standaloneSetup(new DemoController()).build();
+    }
 
+    @Test   
+    public void main() throws Exception {
+      // mockMvc.perform(get("/bolaji"))
+      //     .andExpect(status().isOk())
+      //     .andExpect(view().name("bolaji"));
+    }
+  }
 
-
-
-  //  Assert.assertEquals(); 
 }
-}
+  
