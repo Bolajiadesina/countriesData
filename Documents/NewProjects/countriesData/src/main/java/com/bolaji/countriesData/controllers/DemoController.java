d7576971b16b7020e0d68788f408161f4952e33a
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-04-10 23:49:14 +0100

    fixing frontend issue 2

--- Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/controllers/DemoController.java
+++ Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/controllers/DemoController.java
@@ -2,14 +2,25 @@ package com.bolaji.countriesData.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class DemoController {
-    @GetMapping(value ="bolaji")
+    @GetMapping(value ="/bolaji")
      public String bolajiPage() {  
        
       return "bolajiPortfolio";
+     }
+
+       @GetMapping(value ="/myportfolio")
+     public ModelAndView bolajiPortfolio() {  
+
+        ModelAndView md= new ModelAndView();
+
+        md.setViewName("myportfolio");
+       
+      return md;
      }
 }
