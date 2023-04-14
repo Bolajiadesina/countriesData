94e2c954d12664b45926121e0ad416eb0476b26d
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-04-14 23:59:49 +0100

    vvvv

---
+++ Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/models/PortFolioModel.java
@@ -0,0 +1,10 @@
+package com.bolaji.countriesData.models;
+
+public class PortFolioModel {
+    private String  name;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private String executiveProfile;
+    private String 
+}
