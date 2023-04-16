82f98ec8c13dc6493f9bd58b7291f92c2ac319cd
Bolajiadesina <adesina.bolaji@yahoo.com>
2023-04-16 13:19:49 +0100

    building my port folio

--- Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/models/PortFolioModel.java
+++ Documents/NewProjects/countriesData/src/main/java/com/bolaji/countriesData/models/PortFolioModel.java
@@ -1,10 +1,15 @@
 package com.bolaji.countriesData.models;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class PortFolioModel {
     private String  name;
-    private String firstName;
-    private String lastName;
+    private String fName;
+    private String lName;
     private String role;
     private String executiveProfile;
-    private String 
+    
 }
